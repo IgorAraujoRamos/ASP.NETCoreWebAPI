9569eb698d910251cfd0fd62eafa16eb1b341b41
IgorAraujoRamos <60323890+IgorAraujoRamos@users.noreply.github.com>
2021-09-29 23:26:53 -0300

    Configurando Banco de Dados
    Configurando o banco de dados e executando o Migration

--- Seção 01/01_Fim/MimicAPI/MimicAPI/MimicAPI/Startup.cs
+++ Seção 01/01_Fim/MimicAPI/MimicAPI/MimicAPI/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MimicAPI.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,11 @@ namespace MimicAPI
         {
             services.AddRazorPages();
             services.AddMvc();
+            services.AddDbContext<MimicContext>(opt =>
+                {
+                    opt.UseSqlite("Data Source=Database\\Mimic.db");
+                }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
