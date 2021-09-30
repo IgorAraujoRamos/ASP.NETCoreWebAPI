327609f709507935c5ca1e178c9ff8c7d27af464
IgorAraujoRamos <60323890+IgorAraujoRamos@users.noreply.github.com>
2021-09-29 22:57:24 -0300

    Implementando Modelo Palavra
    Implementando o modelo a ser utilizado na API da entidade Palavra e adicionando o padrão MvC ao sistema

--- Seção 01/01_Fim/MimicAPI/MimicAPI/MimicAPI/Startup.cs
+++ Seção 01/01_Fim/MimicAPI/MimicAPI/MimicAPI/Startup.cs
@@ -24,6 +24,7 @@ namespace MimicAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +43,7 @@ namespace MimicAPI
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseMvc();
             app.UseRouting();
 
             app.UseAuthorization();
