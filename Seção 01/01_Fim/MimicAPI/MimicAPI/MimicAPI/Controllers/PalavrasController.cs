ad03784a537bd505c4fa2d46c4439623efcb98b6
IgorAraujoRamos <60323890+IgorAraujoRamos@users.noreply.github.com>
2021-09-29 23:56:47 -0300

    Definindo as rotas no PalavrasController
    Definindo as rotas diretamente no PalavrasController

--- Seção 01/01_Fim/MimicAPI/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
+++ Seção 01/01_Fim/MimicAPI/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 
 namespace MimicAPI.Controllers
 {
+
+    [Route("api/palavras")]
+
     public class PalavrasController : ControllerBase
     {
 
@@ -18,17 +21,29 @@ namespace MimicAPI.Controllers
             _banco = banco;
         }
 
+        /// <summary>
+        /// /api/palavras
+        /// </summary>
+        /// <returns></returns>
+        [Route("")]
+        [HttpGet]
         public ActionResult ObterTodas()
         {
             return Ok(_banco.Palavras);
         }
-        
+
         //CRUD
+        // /api/palavras/1
+        [Route("{id}")]
+        [HttpGet]
         public ActionResult Obter(int id)
         {
             return Ok(_banco.Palavras.Find(id));
         }
 
+        // -- /api/palavras(post: id, nome, ativo, pontuacao, criacao)
+        [Route("")]
+        [HttpPost]
         public ActionResult Cadastrar(Palavra palavra)
         {
             _banco.Palavras.Add(palavra);
@@ -36,13 +51,20 @@ namespace MimicAPI.Controllers
             return Ok();
         }
 
+        // -- /api/palavras(put: id, nome, ativo, pontuacao, criacao)
+        [Route("{id}")]
+        [HttpPut]
         public ActionResult Atualizar(int id, Palavra palavra)
         {
+            palavra.Id = id;
             _banco.Palavras.Update(palavra);
 
             return Ok();
         }
 
+        // -- /api/palavras/1 (DELETE)
+        [Route("{id}")]
+        [HttpDelete]
         public ActionResult Deletar(int id)
         {
             _banco.Palavras.Remove(_banco.Palavras.Find(id));
